19812b050cb37b00f27ab370c70c84b629e947cc
Saketh Katari <katari15045@iiitd.ac.in>
2017-10-03 00:12:22 +0530

    Able to encode the location_string

--- steganography.py
+++ steganography.py
@@ -11,7 +11,6 @@ def get_binary_list(string):
 		# format(int, '#(no.of digits)') i.e to maintain 7+2 bits and avoiding 0b11 for 3 instead it gives 0b0000011
 		binary_value = format(ascii_value, '#09b')
 		binary_list.append(binary_value)
-	print(binary_list)
 	return binary_list
 
 def print_image_array(image_array):
@@ -33,8 +32,7 @@ def get_location_for_secret_message(secret_binary_list, image_array):
 			current_location = get_location_for_a_secret_bit(binary_string, image_array, current_index)
 			location_array.append(current_location)
 			current_index = current_index + 1
-	location_string = str(location_array)
-	return location_string
+	return location_array
 
 def get_image_array_bit_index(image_array, row, col, color_index, target_bit):
 	target_bit = int(target_bit)
@@ -68,7 +66,10 @@ def get_location_for_a_secret_bit(binary_string, image_array, binary_string_inde
 
 def decode_secret_message_location(image_array, secret_message_location):
 	secret_message_binary_string = []
+	splitted_str = secret_message_location.split("], [")
 	for element in secret_message_location:
+		print(str(element)),
+		continue
 		current_row = element[0]
 		current_col = element[1]
 		current_color_index = element[2]
@@ -76,16 +77,32 @@ def decode_secret_message_location(image_array, secret_message_location):
 		target_color_value = image_array[current_row][current_col][current_color_index]
 		target_color_binary_string = format(target_color_value, '#09b')
 		target_bit = target_color_binary_string[current_color_bit_index]
+		secret_message_binary_string.append(target_bit)
+
+def encode_location_array(secret_message_location):
+	encoded_string = ""
+	for element in secret_message_location:
+		for integer in element:
+			str_integer = str(integer)
+			encoded_string = encoded_string + str_integer
+			encoded_string = encoded_string + ","
+		# removing last character of the string
+		encoded_string = encoded_string[:-1]
+		encoded_string = encoded_string + "|"
+	encoded_string = encoded_string[:-1]
+	return encoded_string
 
 secret_message = "sak@234"
 secret_binary_list = get_binary_list(secret_message)
-print("secret binary list : " + str(secret_binary_list))
+#print("secret binary list : " + str(secret_binary_list))
 
 # cv2.imread(image) gives you BGR, instead of RGB values
 image_array = cv2.imread("kaki.jpg")
 secret_message_location = get_location_for_secret_message(secret_binary_list, image_array)
-print("Size : " + str(len(secret_message_location)) )
-print( "location of secret message : \n" +  secret_message_location)
+#print("Size : " + str(len(secret_message_location)) )
+print( "location array : \n" +  str(secret_message_location) )
+encoded_location_string = encode_location_array(secret_message_location)
+print("encoded location string : \n" + encoded_location_string)
 
 #decoded_message = decode_secret_message_location(image_array, secret_message_location)
 #print("Decoded message : " + decoded_message)
