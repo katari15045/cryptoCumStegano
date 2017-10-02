e4c7e9601381b2977a9d8a5813939b30c7412fcf
Saketh Katari <katari15045@iiitd.ac.in>
2017-10-02 23:49:41 +0530

    During encoding now uniform binary string length is maintained i.e if 3 is converted to binary, - previously it was converted to 0b11 and now- it's 0b0000011

--- steganography.py
+++ steganography.py
@@ -8,8 +8,10 @@ def get_binary_list(string):
 	binary_list = []
 	for character in string:
 		ascii_value = ord(character)
-		binary_value = bin(ascii_value)
+		# format(int, '#(no.of digits)') i.e to maintain 7+2 bits and avoiding 0b11 for 3 instead it gives 0b0000011
+		binary_value = format(ascii_value, '#09b')
 		binary_list.append(binary_value)
+	print(binary_list)
 	return binary_list
 
 def print_image_array(image_array):
@@ -37,7 +39,7 @@ def get_location_for_secret_message(secret_binary_list, image_array):
 def get_image_array_bit_index(image_array, row, col, color_index, target_bit):
 	target_bit = int(target_bit)
 	color_value = image_array[row][col][color_index]
-	binary_color_value = bin(color_value)
+	binary_color_value = format(color_value, '#09b')
 	current_index = 2
 	max_index = len(binary_color_value) - 1
 	found_status = False
@@ -64,10 +66,26 @@ def get_location_for_a_secret_bit(binary_string, image_array, binary_string_inde
 	location_array = [ random_row, random_col, random_color, color_binary_bit_index ]
 	return location_array
 
+def decode_secret_message_location(image_array, secret_message_location):
+	secret_message_binary_string = []
+	for element in secret_message_location:
+		current_row = element[0]
+		current_col = element[1]
+		current_color_index = element[2]
+		current_color_bit_index = element[3]
+		target_color_value = image_array[current_row][current_col][current_color_index]
+		target_color_binary_string = format(target_color_value, '#09b')
+		target_bit = target_color_binary_string[current_color_bit_index]
+
 secret_message = "sak@234"
 secret_binary_list = get_binary_list(secret_message)
+print("secret binary list : " + str(secret_binary_list))
 
 # cv2.imread(image) gives you BGR, instead of RGB values
 image_array = cv2.imread("kaki.jpg")
 secret_message_location = get_location_for_secret_message(secret_binary_list, image_array)
+print("Size : " + str(len(secret_message_location)) )
 print( "location of secret message : \n" +  secret_message_location)
+
+#decoded_message = decode_secret_message_location(image_array, secret_message_location)
+#print("Decoded message : " + decoded_message)
